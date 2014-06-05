4361ac3ad0657cb5dc2c4497a66595af378ef763
Matt Gadda <mgadda@gmail.com>
2014-06-05 08:22:29 -0700

    Initial Commit

---
+++ nth/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  nth
+//
+//  Created by Matt Gadda on 6/5/14.
+//  Copyright (c) 2014 Matt Gadda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
