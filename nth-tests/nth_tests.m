24486b9bb55d4be2cf78401cb2861cf0dd1c2a58
Matt Gadda <mgadda@gmail.com>
2014-06-28 18:11:53 -0700

    adding tests with known failures

---
+++ nth-tests/nth_tests.m
@@ -0,0 +1,34 @@
+//
+//  nth_tests.m
+//  nth-tests
+//
+//  Created by Matt Gadda on 6/28/14.
+//  Copyright (c) 2014 Matt Gadda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface nth_tests : XCTestCase
+
+@end
+
+@implementation nth_tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
