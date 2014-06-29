96de7039f3170f9f37a514580b4e96c012611fb9
Matt Gadda <mgadda@gmail.com>
2014-06-28 17:33:45 -0700

    still debugging

---
+++ nth-test/nth_test.m
@@ -0,0 +1,34 @@
+//
+//  nth_test.m
+//  nth-test
+//
+//  Created by Matt Gadda on 6/28/14.
+//  Copyright (c) 2014 Matt Gadda. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface nth_test : XCTestCase
+
+@end
+
+@implementation nth_test
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+  XCTAssert(true, @"asserting truth");
+}
+
+@end
