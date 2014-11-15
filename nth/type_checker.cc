22c0338075f6fffa052c817eba13df231b3c9562
Matt Gadda <mgadda@gmail.com>
2014-11-15 16:23:13 -0700

    add type_checker.h/cc

---
+++ nth/type_checker.cc
@@ -0,0 +1,9 @@
+//
+//  type_checker.cc
+//  nth
+//
+//  Created by Matt Gadda on 11/6/14.
+//  Copyright (c) 2014 Matt Gadda. All rights reserved.
+//
+
+#include "type_checker.h"
---
+++ nth/type_checker.h
@@ -0,0 +1,14 @@
+//
+//  type_checker.h
+//  nth
+//
+//  Created by Matt Gadda on 11/6/14.
+//  Copyright (c) 2014 Matt Gadda. All rights reserved.
+//
+
+#ifndef __nth__type_checker__
+#define __nth__type_checker__
+
+#include <stdio.h>
+
+#endif /* defined(__nth__type_checker__) */
